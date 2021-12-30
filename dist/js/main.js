5ed46dfbec08d220ff6a50e0a8ddeb1a44902411
modifikult <modifikult2016@gamil.com>
2021-12-30 18:10:05 +0200

    create main page and added animation

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,6 +13,20 @@ new Swiper('.hero-swiper', {
     autoplay: true,
 });
 
+new Swiper('.seo-swiper', {
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    direction: 'horizontal',
+    loop: true,
+    speed: 1000,
+    navigation: {
+        prevEl: '.seo-swiper .swiper-button-prev',
+        nextEl: '.seo-swiper .swiper-button-next',
+    }
+});
+
 
 // Функия работы dropdown-input
 const dropdownInput = document.querySelectorAll('.dropdown-input')
@@ -54,7 +68,6 @@ if (!dropdownInput.length) {
 
     window.addEventListener('click', function (e) {
         if (!e.target.closest('.dropdown-input')) {
-            console.log('worked')
 
             document.querySelectorAll('.dropdown-input').forEach(input => {
                 input.classList.remove('open')
@@ -72,7 +85,7 @@ if (!accordion.length) {
 } else {
     accordion.forEach(accordionTitle => {
         accordionTitle.querySelector('.accordion-title').addEventListener('click', function (e) {
-            e.preventDefault()
+            e.stopPropagation()
             const accordionContent = accordionTitle.querySelector('.accordion-content')
 
             if (!this.classList.contains('open')) {
@@ -84,5 +97,88 @@ if (!accordion.length) {
                 accordionContent.style.maxHeight = null
             }
         })
+    })
+}
+
+// Анимация карты
+const map = document.querySelectorAll('.map svg')
+
+if (!map.length) {
+
+} else {
+    map.forEach(elem => {
+        elem.querySelectorAll('path').forEach(country => {
+            country.addEventListener('mousemove', function (elem) {
+                const description = document.querySelector('.map .description')
+                description.querySelector('.circle').style.animationName = 'circle'
+                description.querySelector('.circle-shadow').style.animationName = 'circleShadow'
+                description.querySelector('.line').style.animationName = 'line'
+                description.querySelector('.text').style.animationName = 'fade'
+
+                const pos = this.getBBox();
+                const x = pos.x + (pos.width / 2.3)
+                const y = pos.y + (pos.width / 4)
+
+                description.style.left = x + 'px'
+                description.style.top = y + 'px'
+
+                description.querySelector('.name').innerText = this.dataset.name
+                description.querySelector('.address').innerText = this.dataset.address
+
+                console.log(pos)
+                if (pos.x < 400) {
+                    description.classList.add('description-revert')
+                } else {
+                    description.classList.remove('description-revert')
+                }
+            })
+            country.addEventListener('mouseleave', function () {
+                const descriptionAllItems = document.querySelectorAll('.map .description *')
+                descriptionAllItems.forEach(item => item.style.animationName = 'none')
+            })
+        })
+    })
+}
+
+// Функия работы tabs
+
+const tabs = document.querySelectorAll('.tab-btn a')
+
+if (!tabs.length) {
+
+} else {
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function (e) {
+            e.preventDefault()
+
+            document.querySelectorAll('.tab-content > div').forEach(elem => elem.classList.remove('show'))
+
+            const id = this.getAttribute('href').replace('#', '')
+
+            if (!this.classList.contains('active')) {
+                tabs.forEach(item => item.classList.remove('active'))
+                this.classList.add('active')
+            }
+
+            document.getElementById(id).classList.add('show')
+
+
+        })
+    })
+}
+
+const icons = document.querySelectorAll('.hero-icons svg path')
+
+if (!icons.length) {
+
+} else {
+    let time = 0
+
+    icons.forEach(elem => {
+        time = time + 500
+        const length1 = elem.getTotalLength()
+        elem.style.strokeDashoffset = length1
+        elem.style.strokeDasharray = length1
+        elem.style.animationDelay = time
     })
 }
